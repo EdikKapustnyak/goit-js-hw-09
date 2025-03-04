9eb8e3b62a4d3f92bdb4576aa97071729dcdb3b8
kapustnyak <zned003@gmail.com>
2025-03-04 11:12:38 +0100

    1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,37 +1,41 @@
 const form = document.querySelector('.feedback-form');
 const textArea = document.querySelector('textarea');
 const input = document.querySelector('input[type="email"]');
-const STORAGE_KEY = 'feedback-form-state'; // ключ для хранения данных
+const STORAGE_KEY = 'feedback-form-state';
 
-form.addEventListener('input', saveFormData);
-form.addEventListener('submit', submitForm);
+let formData = {
+  email: '',
+  message: '',
+};
 
-function saveFormData() {
-  const dataForm = {
-    email: input.value.trim(),
-    message: textArea.value.trim(),
-  };
-  //   console.log(dataForm);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  input.value = formData.email || '';
+  textArea.value = formData.message || '';
 }
 
-const checkForm = localStorage.getItem(STORAGE_KEY);
-if (checkForm) {
-  const parsedData = JSON.parse(checkForm);
-  input.value = parsedData.email || '';
-  textArea.value = parsedData.message || '';
-}
-
-function submitForm(event) {
-  event.preventDefault();
-  const sendFormData = {
-    email: input.value.trim(),
-    message: textArea.value.trim(),
-  };
-  if (sendFormData.email === '' || sendFormData.message === '') {
-    return alert('Fill please all fields');
+form.addEventListener('input', (event) => {
+  if (event.target === input) {
+    formData.email = input.value.trim();
+  } else if (event.target === textArea) {
+    formData.message = textArea.value.trim();
   }
-  console.log(sendFormData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    return alert('Please fill in all fields');
+  }
+  console.log(formData);
+
   localStorage.removeItem(STORAGE_KEY);
   form.reset();
-}
+
+  formData = {
+    email: '',
+    message: '',
+  };
+});
